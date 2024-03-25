b1819017153c1ac2976f6e8422e59838e0f06773
최종규 <153177760+jonggu12@users.noreply.github.com>
2024-03-25 23:23:00 +0900

    Update food2vec.py

--- food2vec.py
+++ food2vec.py
@@ -10,6 +10,19 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from sklearn.metrics.pairwise import cosine_similarity
 import random
+import firebase_admin
+from firebase_admin import credentials, db
+from typing import Dict
+import operator
+
+cred = credentials.Certificate('senior-752be-firebase-adminsdk-k8csi-2b1357c54a.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://senior-752be-default-rtdb.firebaseio.com/'
+})
+
+
+
+
 
 # 환경변수 로드
 load_dotenv()
@@ -28,7 +41,9 @@ client = weaviate.Client(
 app = FastAPI()
 
 # 데이터프레임 로드
-df = pd.read_csv('data.csv')  
+df = pd.read_csv('data.csv', encoding='utf-8')
+
+
 
 # Word2Vec 모델 로드
 model = Word2Vec.load("recipe_word2vec_model.model")
@@ -88,6 +103,36 @@ async def get_recipes(recipe_query: RecipeQuery):
 
 
 
+
+
+@app.get("/most_clicked_recipe")
+async def get_most_clicked_recipe():
+    ref = db.reference('user_clicks')
+    clicks = ref.get()
+    if not clicks:
+        raise HTTPException(status_code=404, detail="No clicks data found")
+
+    # 레시피 클릭 수 집계
+    recipe_clicks: Dict[str, int] = {}
+    for click_id, click_info in clicks.items():
+        recipe_name = click_info['recipe_name']
+        if recipe_name in recipe_clicks:
+            recipe_clicks[recipe_name] += 1
+        else:
+            recipe_clicks[recipe_name] = 1
+
+    # 가장 많이 클릭된 레시피 찾기
+    most_clicked_recipe = max(recipe_clicks.items(), key=operator.itemgetter(1))[0]
+    return {"most_clicked_recipe": most_clicked_recipe}
+
+
+
+
+
+
+
+
+
 # 문서 벡터 리스트 생성 함수
 def get_document_vectors(document_list):
     document_embedding_list = []
@@ -113,33 +158,62 @@ def calculate_cosine_similarity(document_vectors):
 
 cosine_similarities = calculate_cosine_similarity(document_vectors)
 
-@app.get("/recommendations/{title}")
-async def get_recommendations(title: str):
+@app.get("/recommendations/most_clicked")
+async def get_most_clicked_recommendations():
     try:
-        recommendations = recommend_function(title)
+        # 가장 많이 클릭된 레시피 이름 조회
+        most_clicked_recipe = await get_most_clicked_recipe()
+        most_clicked_recipe_name = most_clicked_recipe["most_clicked_recipe"]
+        
+        # 유사한 레시피 추천
+        recommendations = recommend_function(most_clicked_recipe_name)
         content = jsonable_encoder({"recommendations": recommendations})
         return JSONResponse(content=content)
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
 
-def recommend_function(title):
-    indices = pd.Series(df.index, index=df['recipe_name']).drop_duplicates()
-    if title not in indices:
-        raise ValueError("Recipe not found.")
-    idx = indices[title]
+def recommend_function(most_clicked_recipe, num_recommendations=5):
+    
+
+    # cleaned_recipe를 사용하여 데이터셋 내 레시피 존재 여부 확인
+    if most_clicked_recipe not in df['recipe_name'].values:
+        raise ValueError("Recipe not found in the dataset.")
+    
+    idx = df.index[df['recipe_name'] == most_clicked_recipe].tolist()[0]
     sim_scores = list(enumerate(cosine_similarities[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:6]
+    
+
+    sim_scores = sim_scores[0:]
+    
+    # 선택된 레시피들의 인덱스
     recipe_indices = [i[0] for i in sim_scores]
-    recommend = df.iloc[recipe_indices]
-    recommendations = recommend[['recipe_name', 'summary', 'ingredient_name', 'full_step', 'category', 'recipe_image_link']].to_dict('records')
-    return recommendations
+    
+    # 선택된 레시피들의 정보
+    recommendations = df.iloc[recipe_indices]
+    recommendations = recommendations[['recipe_name', 'summary', 'ingredient_name', 'full_step', 'category', 'recipe_image_link']].to_dict('records')
+    
+    # 최대 num_recommendations 개수까지 레시피 추천
+    if len(recommendations) < num_recommendations:
+        additional_recipes = df.sample(n=num_recommendations-len(recommendations)).to_dict('records')
+        recommendations.extend(additional_recipes)
+
+    return recommendations[:num_recommendations]
+    
 
 
 
 
 
 
+
+
+
+    
+    
+    
+# 랜덤 레시피 추천
+    
 
 class RecipeResponse(BaseModel):
     recipe_name: str
